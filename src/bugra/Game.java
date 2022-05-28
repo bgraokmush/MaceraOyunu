cc154bb5462fa33930471a5eea05aef19015a966
Bugra Okumus <saltuk.bugraokumus@gmail.com>
2022-05-28 14:53:15 +0300

    login method is writed to game class

--- src/bugra/Game.java
+++ src/bugra/Game.java
@@ -1,4 +1,16 @@
 package bugra;
 
+import java.util.Scanner;
+
 public class Game {
+    Player player;
+    Location location;
+
+    void login(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Macera Oyununa Hoş Geldin! \n" +
+                "Oyna Başlamadan Önce Adını Girmelisin: ");
+        String playerName = scanner.nextLine();
+        Player player = new Player(playerName);
+    }
 }
