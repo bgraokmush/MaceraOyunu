a9008260fc87ea2a4d11da95146ff0a283a02958
Bugra Okumus <saltuk.bugraokumus@gmail.com>
2022-05-28 14:56:59 +0300

    Necessary constructor pattern is created.

--- src/bugra/Player.java
+++ src/bugra/Player.java
@@ -3,4 +3,57 @@ package bugra;
 public class Player {
     private int damage,health,money;
     private String name, characterName;
+    private Inventory inventory;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
 }
