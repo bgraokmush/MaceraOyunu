bb149bc0a440443b885a0e4a81a7f47f047fa022
Bugra Okumus <saltuk.bugraokumus@gmail.com>
2022-05-28 14:55:48 +0300

    Calling the login method from game class is finished.

--- src/bugra/Main.java
+++ src/bugra/Main.java
@@ -1,8 +1,11 @@
 package bugra;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Game game = new Game();
+        game.login();
     }
 }
